c4a867c4210abb0de230f633d391f8596b8c8391
AndreP7Magno <andremagno14@gmail.com>
2017-12-08 23:51:34 -0200

    novo commit e2

--- Compiladores Etapa E2/Compiladores Etapa E2/Program.cs
+++ Compiladores Etapa E2/Compiladores Etapa E2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Antlr4.Runtime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace Compiladores_Etapa_E2
     {
         static void Main(string[] args)
         {
+            AntlrInputStream inputStream = new AntlrInputStream(Console.In);
+
+            GramaticaLexer lexer = new GramaticaLexer(inputStream);
+
+
+            CommonTokenStream bts = new CommonTokenStream(lexer);
+            bts.Fill();
+            IToken tk = lexer.NextToken();
+
+            GramaticaParser p = new GramaticaParser(bts);
+            p.program();
+            Console.ReadKey();
         }
     }
 }
